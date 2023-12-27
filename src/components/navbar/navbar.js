8a18153701f9c19d13860efaf463dbc648605834
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2023-12-27 15:46:18 -0300

    start

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './navbar.css';
+
+function Navbar() {
+    return(
+        <div>
+            Hello World
+        </div>
+    );
+}
+
+export default Navbar;
