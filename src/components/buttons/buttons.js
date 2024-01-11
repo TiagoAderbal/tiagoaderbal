c9faa0bf8f4121eae43c1c013fbedd682761627b
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2024-01-11 10:17:21 -0300

    ajustado className component button

--- src/components/buttons/buttons.js
+++ src/components/buttons/buttons.js
@@ -5,10 +5,10 @@ import './buttons.css';
 function Buttons() {
     return (
         <div>
-            <a className='whats' href='https://wa.me/5514998761501?text=Ol%C3%A1%2C+gostaria+de+ter+um+site.+' target='blank'><i class="bi bi-whatsapp"></i></a>
-            <a className='link' href='https://www.linkedin.com/in/tiago-aderbal-francisco-b0a67217b' target='blank'><i class="bi bi-linkedin"></i></a>
-            <a className='gith' href='https://github.com/TiagoAderbal' target='blank'><i class="bi bi-github"></i></a>
-            <a className='insta' href='https://www.instagram.com/tiago.aderbal?igsh=ZGNjOWZkYTE3MQ==' target='blank'><i class="bi bi-instagram"></i></a>
+            <a className='whats' href='https://wa.me/5514998761501?text=Ol%C3%A1%2C+gostaria+de+ter+um+site.+' target='blank'><i className="bi bi-whatsapp"></i></a>
+            <a className='link' href='https://www.linkedin.com/in/tiago-aderbal-francisco-b0a67217b' target='blank'><i className="bi bi-linkedin"></i></a>
+            <a className='gith' href='https://github.com/TiagoAderbal' target='blank'><i className="bi bi-github"></i></a>
+            <a className='insta' href='https://www.instagram.com/tiago.aderbal?igsh=ZGNjOWZkYTE3MQ==' target='blank'><i className="bi bi-instagram"></i></a>
         </div>
     );
 }
