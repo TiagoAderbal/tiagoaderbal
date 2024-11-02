ba8479d4ee0f32988c267d827aa87d43fe4a0014
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2024-11-02 20:24:59 -0300

    ajustado responsividade

--- src/components/ButProj/ButProj.js
+++ src/components/ButProj/ButProj.js
@@ -5,19 +5,28 @@ import { Link } from "react-router-dom";
 
 function ButProj() {
   return (
-    <div className="d-flex justify-content-center align-items-center" style={{ marginBottom: "5rem" }}>
-      <Link className="button_projects me-2" to="/projects">
-        MEUS PROJETOS
-      </Link>
-      <img
-        id="arrow"
-        src="./assets/imgs/arched-arrow.svg"
-        alt="arrow"
-        className="me-2 mt-4"
-      />
-      <p id="text-nav" style={{ transform: "rotate(15deg)" }} className="mt-2">
-        Clique aqui
-      </p>
+    <div className="container pe-5 ps-5">
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ marginBottom: "5rem" }}
+      >
+        <Link className="button_projects" to="/projects">
+          MEUS PROJETOS
+        </Link>
+        <img
+          id="arrow"
+          src="./assets/imgs/arched-arrow.svg"
+          alt="arrow"
+          className="mt-4"
+        />
+        <p
+          id="text-nav"
+          style={{ transform: "rotate(15deg)" }}
+          className="mt-2"
+        >
+          Clique aqui
+        </p>
+      </div>
     </div>
   );
 }
